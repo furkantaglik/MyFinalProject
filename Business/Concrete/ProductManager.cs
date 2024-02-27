f785c24b14fa20a2d4c98d1e462fc9af3d225961
furkantaglik <furkantaglik2334@gmail.com>
2024-02-27 16:51:19 +0300

    ASPECT

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,11 +1,16 @@
 ﻿using Business.Abstract;
 using Business.Constants;
+using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Validation;
+using Core.CrossCC.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
 using Entities.DTOs;
+using FluentValidation;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace Business.Concrete
@@ -19,14 +24,10 @@ namespace Business.Concrete
 			_productDal = productDal;
 		}
 
+		[ValidationAspect(typeof(ProductValidator))]
 		public IResult Add(Product product)
 		{
-			if (product.ProductName.Length < 2)
-			{
-				return new ErrorResult(Messages.ProductNameInvalid);
-			}
-
-
+			ValidationTool.Validate(new ProductValidator(), product);
 			_productDal.Add(product);
 			return new SuccessResult(Messages.ProductAdded);
 		}
---
+++ Core/CrossCC/Validation/ValidationTool.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.CrossCC.Validation
+{
+	public static class ValidationTool
+	{
+		public static void Validate(IValidator validator, object entity)
+		{
+			var context = new ValidationContext<object>(entity);
+			var result = validator.Validate(context);
+			if (!result.IsValid)
+			{
+				throw new ValidationException(result.Errors);
+			}
+		}
+	}
+}
